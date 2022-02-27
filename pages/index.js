12c8c1f4486b23d89dcb0464d691a0aecb20abd9
Ngopoot <95578190+coutrex@users.noreply.github.com>
2022-02-27 11:50:28 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
-        </p>
+         for more information visit site : <a href="ngopoot.com">ngopoot.com</a></p>
       </main>
 
       <Footer />
